cc3bb32731fcc3b89c6343db06e138e6054f14c2
Longhao Tan <492799962@qq.com>
2024-12-05 11:13:50 -0500

    Update LeNet-5-int8.py

--- model/LeNet-5-int8.py
+++ model/LeNet-5-int8.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-# 加载预训练模型
+# Load pre-trained model
 model = tf.keras.models.load_model('LeNet-5.h5')
 
 # Prepare the calibration dataset (for calibrating the quantization range)
@@ -9,24 +9,24 @@ x_test = x_test.astype("float32") / 255.0
 x_test = x_test[:100]  # only use first 100 datas
 x_test = x_test.reshape(-1, 28, 28, 1)
 
-# 创建量化模型的转换器
+# Create quantization converter
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 
-# 启用量化优化
+# Initial optimization
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 
-# 定义校准数据生成器
+# Define calibration data generator
 def representative_dataset():
     for data in x_test:
-        yield [data.reshape(1, 28, 28, 1)]  # 确保形状为 (1, 28, 28, 1)
+        yield [data.reshape(1, 28, 28, 1)]
 
-# 设置校准数据集
+# Set calibration dataset
 converter.representative_dataset = representative_dataset
 
-# 设置目标数据类型为 int8
+# Set target data type to int8
 converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-converter.inference_input_type = tf.int8  # 输入量化为 int8
-converter.inference_output_type = tf.int8  # 输出量化为 int8
+converter.inference_input_type = tf.int8  # Quantize input to int8
+converter.inference_output_type = tf.int8  # Quantize output to int8
 
 # convert
 tflite_int8_model = converter.convert()
